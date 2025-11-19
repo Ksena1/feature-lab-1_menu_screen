b10207ebd00effc8e0b2a38af87f331169e02c72
Ksena1 <146750455+Ksena1@users.noreply.github.com>
2025-11-19 07:51:19 +0000

    feat: add import for coffee shop list screen in menu

--- my_flutter_app/lib/screens/menu_screen.dart
+++ my_flutter_app/lib/screens/menu_screen.dart
@@ -6,6 +6,7 @@ import '../data/mock_products.dart';
 import '../models/product.dart';
 import '../providers/cart_provider.dart';
 import 'cart_screen.dart';
+import '../features/map/presentation/screens/coffee_shop_list_screen.dart';
 
 class MenuScreen extends StatefulWidget {
   const MenuScreen({Key? key}) : super(key: key);
