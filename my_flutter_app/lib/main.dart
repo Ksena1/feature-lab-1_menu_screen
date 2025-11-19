79b8bde1ceb311ac1709850e316710f6507e175b
Ksena1 <146750455+Ksena1@users.noreply.github.com>
2025-11-19 07:34:19 +0000

    feat: integrate address provider and button into app

--- my_flutter_app/lib/main.dart
+++ my_flutter_app/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/menu_screen.dart';
 import 'providers/cart_provider.dart';
+import 'features/map/providers/address_provider.dart';
+import 'features/map/data/database/app_database.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => CartProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CartProvider()),
+        // ДОБАВЬТЕ ПРОВАЙДЕР АДРЕСА:
+        ChangeNotifierProvider(create: (context) => AddressProvider()),
+        // ДОБАВЬТЕ ПРОВАЙДЕР БАЗЫ ДАННЫХ:
+        Provider(create: (context) => AppDatabase()),
+      ],
       child: MaterialApp(
         title: 'Кофейня "Уютная"',
         theme: ThemeData(
@@ -29,7 +37,6 @@ class MyApp extends StatelessWidget {
             foregroundColor: Colors.white,
             elevation: 2,
           ),
-          // УБИРАЕМ cardTheme полностью - он не обязателен
           useMaterial3: true,
         ),
         home: const MenuScreen(),
